8e95d1ea50762d5df85d366ee3df6fd899cbf3c8
“Your <fikri.alichsan@gmail.com>
2022-10-12 05:43:04 +0800

    create history konseling screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hmif_care_mobile/Screens/History/historyScreen.dart';
 import 'package:hmif_care_mobile/Screens/Homepage/homepage_screen.dart';
 import 'package:hmif_care_mobile/Screens/Login/login_secreen.dart';
 import 'package:hmif_care_mobile/constants.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: HomePageScreen(),
+      home: HistoryScreen(),
     );
   }
 }
